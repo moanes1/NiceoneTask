9ac2c4777875c3ad697b73276caf87cc4fa51383
mohamed moanes <mohamed.moanes@easyschools.org>
2021-03-10 17:25:19 +0200

    adding main view model

--- app/src/main/java/com/moanes/niceonetask/ui/MainActivity.kt
+++ app/src/main/java/com/moanes/niceonetask/ui/MainActivity.kt
@@ -2,11 +2,14 @@ package com.moanes.niceonetask.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
 import com.moanes.niceonetask.R
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    val viewModel: MainViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
--- app/src/main/java/com/moanes/niceonetask/ui/MainViewModel.kt
+++ app/src/main/java/com/moanes/niceonetask/ui/MainViewModel.kt
@@ -1,13 +1,16 @@
 package com.moanes.niceonetask.ui
 
+import androidx.hilt.lifecycle.ViewModelInject
 import com.moanes.datasource.model.Character
 import com.moanes.datasource.repositories.CharactersRepo
 import com.moanes.niceonetask.base.BaseViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
-class MainViewModel @Inject constructor(private val charactersRepo: CharactersRepo) :
+@HiltViewModel
+class MainViewModel @Inject  constructor(private val charactersRepo: CharactersRepo) :
     BaseViewModel() {
     private var lastOffset = 0
     private val limit = 10
